f5f6acdd11a5369fb6e2ebe9f44d7945ad402144
Rohit singh <rohitpramodsingh@.com>
2025-03-14 15:00:07 +0530

    Updated form layout and added required files

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#      os.path.join(BASE_DIR, 'static'),  # Where your static files are located
-#  ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),  # Where your static files are located
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
